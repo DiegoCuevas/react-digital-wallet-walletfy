904e9db55a1de950b2b545d5186a23bc076776d3
Cesar Cachay <cesarcachay1996@gmail.com>
2019-06-20 18:22:52 -0500

    Show fake data structure

--- src/views/expenses.js
+++ src/views/expenses.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import { listExpense } from "../data/daylistExpense";
+
 function Expenses() {
-  return <h1>Expenses</h1>;
+  const total = [];
+  const years = Object.keys(listExpense);
+  years.forEach(year =>
+    Object.values(listExpense[year]).forEach(month =>
+      month.map(day => total.push(day))
+    )
+  );
+  return (
+    <>
+      <h1>Expenses</h1>
+      <div>
+        {total.map(e => (
+          <div>
+            <span>
+              <h2>{e.categoryId}</h2>
+              <h3>{e.amount}</h3>
+            </span>
+            <h3>{e.description}</h3>
+            <h3>{e.date}</h3>
+            <br />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Expenses;
