da8162799d5b9f1dfde7ac1e2e2b4e4630562404
Diego Cuevas <diegocuevas579@gmail.com>
2019-06-20 17:57:41 -0500

    Add footer

---
+++ src/components/footer.js
@@ -0,0 +1,72 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx, css } from "@emotion/core";
+import { FaMinusSquare } from "react-icons/fa";
+import { FaPlusSquare } from "react-icons/fa";
+import { FaBalanceScale } from "react-icons/fa";
+import { navigate } from "@reach/router";
+
+function Footer() {
+  function handleExpenses() {
+    navigate("/");
+  }
+  function handleExpenses() {
+    navigate("/");
+  }
+  function handleExpenses() {
+    navigate("/");
+  }
+  function handleIncome() {
+    navigate("/incomes");
+  }
+  function handleResult() {
+    navigate("/balances");
+  }
+  const section = {
+    display: "flex",
+    justifyContent: "space-around",
+    background: "#e0e0dc",
+    alignContent: "center"
+  };
+  return (
+    <>
+      <section css={section}>
+        <div
+          onClick={handleExpenses}
+          css={{
+            fontSize: "40px",
+            color: "#b23655",
+            display: "flex",
+            alignItems: "center",
+            cursor: "pointer"
+          }}
+        >
+          <FaMinusSquare />
+        </div>
+        <div
+          onClick={handleIncome}
+          css={{
+            fontSize: "40px",
+            color: "#36b35f",
+            display: "flex",
+            alignItems: "center"
+          }}
+        >
+          <FaPlusSquare />
+        </div>
+        <div
+          onClick={handleResult}
+          css={{
+            fontSize: "40px",
+            color: "blue",
+            display: "flex",
+            alignItems: "center"
+          }}
+        >
+          <FaBalanceScale />
+        </div>
+      </section>
+    </>
+  );
+}
+export default Footer;
--- src/views/expenses.js
+++ src/views/expenses.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Footer from "../components/footer"
 
 function Expenses() {
-  return <h1>Expenses</h1>;
+  return <><h1>Expenses</h1>
+    <Footer/>
+  </>;
 }
 
 export default Expenses;
